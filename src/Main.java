46f8cc3754a302eb00f8451bce418d765d838ea9
zhy-lou <z2929473414@gmail.com>
2026-05-30 20:53:50 +0800

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    System.out.println("hello github");
     }
 
